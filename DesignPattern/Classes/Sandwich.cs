78f3dd9753943b363b9d7cfdc1045878ede200c4
Luigi <luigi.carole@gmail.com>
2022-05-31 17:08:57 +0200

    Avancement du projet

---
+++ DesignPattern/Classes/Sandwich.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Classes
+{
+    public class Sandwich
+    {
+        public Sandwich(string nom, List<string> ingredients, double prix)
+        {
+            this.nom = nom;
+            Ingredients = ingredients;
+            this.prix = prix;
+        }
+
+        public string nom { get; set; }
+        public List<string> Ingredients { get; set; } = new List<string>();
+        public double prix { get; set; }
+    }
+}
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,3 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DesignPattern.Classes;
+Sandwich JambonBeurre = new Sandwich("Jambon Beurre",new List<string> { "pain", "tranche de jambon", "beurre" },3.50);
+Sandwich PouletCrudites = new Sandwich("Poulet crudites", new List<string> { "pain", "oeuf", "tomate", "tranche de poulet", "mayonnaise", "salade" }, 5);
+Sandwich Dieppois = new Sandwich("Dieppois", new List<string> { "pain", "thon", "tomate", "mayonnaise", "salade" }, 4.50);
 
+List<Sandwich> allSandwich = new List<Sandwich> { JambonBeurre, PouletCrudites, Dieppois };
+Console.WriteLine("Saisissez votre commande : ");
+string saisie = Console.ReadLine();
+Dictionary<string, int> detailsCommand = new Dictionary<string, int>();
+
+string[] listSandwich = saisie.Split(',');
+
+//Enlève les blancs en dévut et fin de ligne.
+for(int i = 0; i < listSandwich.Length; i++)
+{
+    listSandwich[i] = listSandwich[i].Trim();
+}
+
+//Enlève les doublons du tableau et incrément la quantité de la première occurence trouvé.
+foreach (string sandwich in listSandwich)
+{
+    string[] tmp = sandwich.Split(' ',2);
+    if (detailsCommand.ContainsKey(tmp[1]))
+    {
+        detailsCommand[tmp[1]] += Int32.Parse(tmp[0]);
+    }
+    else
+    {
+        detailsCommand.Add(tmp[1],Int32.Parse(tmp[0]));
+    }
+}
+
+double prixTotal = 0;
+foreach(KeyValuePair<string, int> kvp in detailsCommand)
+{
+    Console.WriteLine(kvp.Key+" "+kvp.Value);
+    foreach(Sandwich sandwich in allSandwich)
+    {
+        if(sandwich.nom == kvp.Key)
+        {
+            foreach(string ingredient in sandwich.Ingredients)
+            {
+                Console.WriteLine("    " + ingredient);
+            }
+            prixTotal += sandwich.prix * kvp.Value;
+            break;
+        }
+    }
+}
+Console.WriteLine("Prix total = " + Math.Round(prixTotal,2).ToString("N2")+"€");
+
+Console.ReadLine();

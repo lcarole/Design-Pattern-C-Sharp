81cbe0704c929473d2baba19bd2515287adb0caf
Luigi <luigi.carole@gmail.com>
2022-05-31 18:11:04 +0200

    Ajout du contrôle des erreurs

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -5,49 +5,71 @@ Sandwich PouletCrudites = new Sandwich("Poulet crudites", new List<string> { "pa
 Sandwich Dieppois = new Sandwich("Dieppois", new List<string> { "pain", "thon", "tomate", "mayonnaise", "salade" }, 4.50);
 
 List<Sandwich> allSandwich = new List<Sandwich> { JambonBeurre, PouletCrudites, Dieppois };
-Console.WriteLine("Saisissez votre commande : ");
+
+Console.WriteLine("Saisissez votre commande : (Taper Exit ou metter une chaine vide pour quitter)");
 string saisie = Console.ReadLine();
-Dictionary<string, int> detailsCommand = new Dictionary<string, int>();
 
-string[] listSandwich = saisie.Split(',');
-
-//Enlève les blancs en dévut et fin de ligne.
-for(int i = 0; i < listSandwich.Length; i++)
+while (saisie != "Exit")
 {
-    listSandwich[i] = listSandwich[i].Trim();
-}
+    Dictionary<string, int> detailsCommand = new Dictionary<string, int>();
 
-//Enlève les doublons du tableau et incrément la quantité de la première occurence trouvé.
-foreach (string sandwich in listSandwich)
-{
-    string[] tmp = sandwich.Split(' ',2);
-    if (detailsCommand.ContainsKey(tmp[1]))
+    string[] listSandwich = saisie.Split(',');
+
+    //Enlève les blancs en dévut et fin de ligne.
+    for (int i = 0; i < listSandwich.Length; i++)
     {
-        detailsCommand[tmp[1]] += Int32.Parse(tmp[0]);
+        listSandwich[i] = listSandwich[i].Trim();
     }
-    else
-    {
-        detailsCommand.Add(tmp[1],Int32.Parse(tmp[0]));
-    }
-}
 
-double prixTotal = 0;
-foreach(KeyValuePair<string, int> kvp in detailsCommand)
-{
-    Console.WriteLine(kvp.Key+" "+kvp.Value);
-    foreach(Sandwich sandwich in allSandwich)
+    //Enlève les doublons du tableau et incrément la quantité de la première occurence trouvé.
+    foreach (string sandwich in listSandwich)
     {
-        if(sandwich.nom == kvp.Key)
+        string[] tmp = sandwich.Split(' ', 2);
+        try
         {
-            foreach(string ingredient in sandwich.Ingredients)
+            if (detailsCommand.ContainsKey(tmp[1]))
             {
-                Console.WriteLine("    " + ingredient);
+                detailsCommand[tmp[1]] += Int32.Parse(tmp[0]);
             }
-            prixTotal += sandwich.prix * kvp.Value;
-            break;
+            else
+            {
+                detailsCommand.Add(tmp[1], Int32.Parse(tmp[0]));
+            }
+        }
+        catch
+        {
+            Console.WriteLine("La saisie est invalide.");
         }
     }
-}
-Console.WriteLine("Prix total = " + Math.Round(prixTotal,2).ToString("N2")+"€");
 
-Console.ReadLine();
+    double prixTotal = 0;
+    
+    foreach (KeyValuePair<string, int> kvp in detailsCommand)
+    {
+        bool Exists = false;
+        Console.WriteLine(kvp.Key + " " + kvp.Value);
+        foreach (Sandwich sandwich in allSandwich)
+        {
+            if (sandwich.nom == kvp.Key)
+            {
+                foreach (string ingredient in sandwich.Ingredients)
+                {
+                    Console.WriteLine("    " + ingredient);
+                }
+                prixTotal += sandwich.prix * kvp.Value;
+                Exists = true;
+                break;
+            }
+        }
+
+        if (!Exists)
+        {
+            Console.WriteLine(kvp.Key + " n'existe pas.");
+        }
+    }
+    Console.WriteLine("Prix total = " + Math.Round(prixTotal, 2).ToString("N2") + "€");
+
+    Console.WriteLine("Saisissez votre commande : (Taper Exit ou metter une chaine vide pour quitter)");
+    saisie = Console.ReadLine();
+}
+//Console.ReadLine();

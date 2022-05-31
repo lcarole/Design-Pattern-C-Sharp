2e7358f6eeb8835282183bd7557ed6fbc4ce184c
Luigi <luigi.carole@gmail.com>
2022-05-31 15:15:53 +0200

    Premier commit

---
+++ DesignPattern/Sandwich.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    public class Sandwich
+    {
+        private string nom { get; set; }
+        private List<string> Ingredients { get; set; } = new List<string>();
+        private double prix { get; set; }
+    }
+}

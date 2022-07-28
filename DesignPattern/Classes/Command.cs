1e5fd8f624a0fb695a4df6c78447dc888b1e9f09
Kaseriu <arthurmatuszek@gmail.com>
2022-07-28 14:38:21 +0200

    Refacto variable language

---
+++ DesignPattern/Classes/Command.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPattern.Classes
+{
+    public class Command
+    {
+        private List<Sandwich> sandwichList = new List<Sandwich>();
+
+        public Command(List<Sandwich> sandwichList)
+        {
+            this.sandwichList = sandwichList;
+        }
+
+        public List<Sandwich> SandwichList
+        {
+            get { return sandwichList; }
+        }
+    }
+}
